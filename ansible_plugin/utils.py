5c417b1093ba91e67a2ea251226ec683c67a6c74
Mikhail Testov <mihail.testov@riskfocus.com>
2017-03-02 11:53:39 +0200

    cloudify-ansible-plugin by Riskfocus v2.0.5

--- ansible_plugin/utils.py
+++ ansible_plugin/utils.py
@@ -1,5 +1,5 @@
 ########
-# Copyright (c) 2014 GigaSpaces Technologies Ltd. All rights reserved
+# Copyright (c) 2017 Riskfocus LV Ltd. All rights reserved
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,7 +15,9 @@
 
 # Built-in Imports
 import os
+import re
 import tempfile
+import requests
 from subprocess import Popen, PIPE
 
 # Third-party Imports
@@ -27,19 +29,67 @@ from cloudify import exceptions
 CLOUDIFY_MANAGER_PRIVATE_KEY_PATH = 'CLOUDIFY_MANAGER_PRIVATE_KEY_PATH'
 
 
-def get_playbook_path(playbook):
+def _download_playbook_templates(playbook, playbook_local_path):
+    cloudify_url = os.environ['MANAGER_FILE_SERVER_BLUEPRINTS_ROOT_URL']
+    ctx.logger.debug('cloudify_url: {}'.format(cloudify_url))
+    blueprint_id = ctx.blueprint.id
+    ctx.logger.debug('blueprint_id: {}'.format(blueprint_id))
+    cloudify_playbook_url = os.path.dirname(playbook)
+    ctx.logger.debug('cloudify_playbook_url: {}'.format(cloudify_playbook_url))
+    playbook_templates_url = '{}/{}/{}/templates'.format(cloudify_url, blueprint_id, cloudify_playbook_url)
+    ctx.logger.debug('playbook_templates_url: {}'.format(playbook_templates_url))
+    playbook_local_dir = os.path.dirname(playbook_local_path)
+    ctx.logger.debug('playbook_local_dir: {}'.format(playbook_local_dir))
 
+    template_file_pattern = re.compile(r'href=\"(\w.*)\"')
+
+    if os.path.isdir(playbook_local_dir):
+        playbook_templates_local_path = '{}/templates'.format(playbook_local_dir)
+        os.makedirs(playbook_templates_local_path)
+    else:
+        raise exceptions.NonRecoverableError(
+            'Could not find playbook directory {}.'.format(playbook_local_dir))
+
+    directory_listing_response = requests.get('{}'.format(playbook_templates_url))
+    ctx.logger.debug('directory_listing_response: {}'.format(directory_listing_response.text))
+    if directory_listing_response.status_code == requests.codes.ok:
+        ctx.logger.info('{} template folder found on source server:'.format(cloudify_playbook_url))
+
+        for template_file in template_file_pattern.findall(directory_listing_response.content):
+            remote_template_file = '{}/{}'.format(playbook_templates_url, template_file)
+            ctx.logger.debug('remote_template_file: {}'.format(remote_template_file))
+            template_file_response = requests.get(remote_template_file)
+            if template_file_response.status_code == requests.codes.ok:
+                template_file_local_path = '{}/{}'.format(playbook_templates_local_path, template_file)
+                ctx.logger.debug('template_file_local_path: {}'.format(template_file_local_path))
+                try:
+                    template_file_write = open(template_file_local_path, 'wb')
+                    template_file_write.write(template_file_response.content)
+                    template_file_write.close()
+                    ctx.logger.info('Downloaded template {}'.format(template_file_local_path))
+                except:
+                    raise exceptions.NonRecoverableError(
+                        'Issue creating template {}'.format(template_file_local_path))
+            else:
+                raise exceptions.NonRecoverableError(
+                    'Issue downloading template {}'.format(template_file))
+    else:
+        ctx.logger.warn('Template folder for playbook {} not found on source server'.format(playbook))
+
+
+def get_playbook_path(playbook):
     try:
         path_to_file = ctx.download_resource(playbook)
     except exceptions.HttpException as e:
         raise exceptions.NonRecoverableError(
             'Could not get playbook file: {}.'.format(str(e)))
 
+    _download_playbook_templates(playbook, path_to_file)
+
     return path_to_file
 
 
 def get_inventory_path(inventory):
-
     if not inventory:
         inventory.append(ctx.instance.host_ip)
 
@@ -53,7 +103,6 @@ def get_inventory_path(inventory):
 
 
 def get_agent_user(user=None):
-
     if not user:
         if 'user' not in ctx.instance.runtime_properties:
             user = ctx.bootstrap_context.cloudify_agent.user
@@ -67,7 +116,6 @@ def get_agent_user(user=None):
 
 
 def get_keypair_path(key=None):
-
     if not key:
         if 'key' in ctx.instance.runtime_properties:
             key = ctx.instance.runtime_properties['key']
@@ -86,7 +134,6 @@ def get_keypair_path(key=None):
 
 
 def write_configuration_file(config):
-
     home = os.path.expanduser("~")
 
     file_path = os.path.join(home, '.ansible.cfg')
@@ -98,7 +145,6 @@ def write_configuration_file(config):
 
 
 def run_command(command):
-
     try:
         run = Popen(command, stdout=PIPE)
     except Exception as e:
--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 ########
-# Copyright (c) 2014 GigaSpaces Technologies Ltd. All rights reserved
+# Copyright (c) 2017 Riskfocus LV Ltd. All rights reserved
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,13 +17,13 @@
 from setuptools import setup
 
 setup(
-    name='cloudify-ansible-plugin',
-    version='1.3a3',
-    author='Gigaspaces',
-    author_email='cosmo-admin@gigaspaces.com',
+    name='rf-ansible-plugin',
+    version='2.0.5',
+    author='Riskfocus',
+    author_email='riskfocus.com',
     description='Integrates with cloudify to deploy Ansible Playbooks',
     packages=['ansible_plugin'],
     license='LICENSE',
     zip_safe=False,
-    install_requires=["cloudify-plugins-common==3.3a3", "ansible==1.8.2"]
+    install_requires=["cloudify-plugins-common", "ansible", "requests"]
 )
